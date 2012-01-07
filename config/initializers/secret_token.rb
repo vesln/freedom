3aded1369ca7c1aa048f230227cd8e71d16e960d
Veselin Todorov <veselin@veselin.bg>
2012-01-07 18:31:27 +0200

    Adding rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freedom::Application.config.secret_token = '4e249361eedc403186811705562632c6e93746768588fe2d09945416e7360bfd01c2fc0727ca0c3273f368239e42e83011fe530a6939cf209b2cb75f2bb0ae33'
