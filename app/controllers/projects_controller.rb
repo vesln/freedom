a122add14d3644568cb9e7f01e430d7d5e08ef5e
Veselin Todorov <veselin@veselin.bg>
2012-01-11 19:29:22 +0200

    [misc] Removing dummy comments.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,44 +1,35 @@
 class ProjectsController < ApplicationController
-  # GET /projects
-  # GET /projects.json
   def index
     @projects = Project.all
 
     respond_to do |format|
-      format.html # index.html.erb
+      format.html
       format.json { render json: @projects }
     end
   end
 
-  # GET /projects/1
-  # GET /projects/1.json
   def show
     @project = Project.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
+      format.html
       format.json { render json: @project }
     end
   end
 
-  # GET /projects/new
-  # GET /projects/new.json
   def new
     @project = Project.new
 
     respond_to do |format|
-      format.html # new.html.erb
+      format.html
       format.json { render json: @project }
     end
   end
 
-  # GET /projects/1/edit
   def edit
     @project = Project.find(params[:id])
   end
 
-  # POST /projects
-  # POST /projects.json
   def create
     @project = Project.new(params[:project])
 
@@ -53,8 +44,6 @@ class ProjectsController < ApplicationController
     end
   end
 
-  # PUT /projects/1
-  # PUT /projects/1.json
   def update
     @project = Project.find(params[:id])
 
@@ -69,8 +58,6 @@ class ProjectsController < ApplicationController
     end
   end
 
-  # DELETE /projects/1
-  # DELETE /projects/1.json
   def destroy
     @project = Project.find(params[:id])
     @project.destroy
