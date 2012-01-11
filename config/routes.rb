53d866983207bdab189d71c575eef011bc69ed9e
Veselin Todorov <veselin@veselin.bg>
2012-01-11 13:54:48 +0200

    [conf] Updating auth routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Freedom::Application.routes.draw do
   get "dashboards/index"
 
-  devise_for :users
+  devise_scope :user do
+    get "sign_in", :to => "devise/sessions#new"
+    get "sign_out", :to => "devise/sessions#destroy"
+  end
+  
   root :to => 'dashboards#index'
 end
