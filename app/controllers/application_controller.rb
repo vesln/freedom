eb9e5063b4b57073003b469f8f39ce96d8539adb
Veselin Todorov <veselin@veselin.bg>
2012-01-11 20:45:18 +0200

    [ui] Adding admin-only links in the nav.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,10 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
   
   protect_from_forgery
+  
+  protected
+  
+  def authenticate_admin!
+    redirect_back_or_default('/') unless current_user.is_admin?
+  end
 end
