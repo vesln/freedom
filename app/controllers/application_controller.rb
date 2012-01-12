1058126d99cdad48584615858ad7518032cde008
Veselin Todorov <veselin@veselin.bg>
2012-01-12 09:42:26 +0200

    [api] Adding current project helper method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,17 @@
 class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
   
+  helper_method :current_project
+  
   protect_from_forgery
   
   protected
   
   def authenticate_admin!
     redirect_to('/') unless current_user.is_admin?
+  end
+  
+  def current_project
+    @current_project ||= Project.find_by_id(params[:project_id])
   end
 end
