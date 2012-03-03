08fe0731e79a934dc4ba3275e1f0b7f92007de5d
Veselin Todorov <veselin@veselin.bg>
2012-03-03 23:06:44 +0200

    Add current_user helper methods.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :current_user_session, :current_user
+
+   private
+    def current_user_session
+      @current_user_session ||= UserSession.find
+    end
+
+    def current_user
+      @current_user ||= current_user_session && current_user_session.record
+    end
 end
