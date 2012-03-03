27d25c5363a367ae249701480353c2bb5b2537d4
Veselin Todorov <veselin@veselin.bg>
2012-03-03 13:33:47 +0200

    Fresh start.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freedom::Application.config.secret_token = 'ddf97a97de4bc0734a146aaede35b58da9da9f1734ca59491dad53a664c635718634b28a4a8cfa00f8feafa4dc67ccbe54dedbf7b79624dfa3abf1927effc87b'
