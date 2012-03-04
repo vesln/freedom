31dea5798a8498d8190aec4cce77f5293668f94a
Veselin Todorov <veselin@veselin.bg>
2012-03-04 18:55:30 +0200

    Misc.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,16 @@ class ApplicationController < ActionController::Base
   helper_method :logged_in?, :current_user_session, :current_user
 
    private
-    def current_user_session
-      @current_user_session ||= UserSession.find
-    end
 
-    def current_user
-      @current_user ||= current_user_session && current_user_session.record
-    end
+   def current_user_session
+     @current_user_session ||= UserSession.find
+   end
 
-    def logged_in?
-      !!current_user
-    end
+   def current_user
+     @current_user ||= current_user_session && current_user_session.record
+   end
+
+   def logged_in?
+     !!current_user
+   end
 end
