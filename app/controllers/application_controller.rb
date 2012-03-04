4a7bd9f3ed9f3b9f3cf1134b78b1fe60281cbeb8
Veselin Todorov <veselin@veselin.bg>
2012-03-04 18:56:16 +0200

    Fix indentation in `ApplicationController`.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,17 @@ class ApplicationController < ActionController::Base
 
   helper_method :logged_in?, :current_user_session, :current_user
 
-   private
+  private
 
-   def current_user_session
-     @current_user_session ||= UserSession.find
-   end
+  def current_user_session
+    @current_user_session ||= UserSession.find
+  end
 
-   def current_user
-     @current_user ||= current_user_session && current_user_session.record
-   end
+  def current_user
+    @current_user ||= current_user_session && current_user_session.record
+  end
 
-   def logged_in?
-     !!current_user
-   end
+  def logged_in?
+    !!current_user
+  end
 end
