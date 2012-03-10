ec5d56d6875b522f4eb89feb9e2cdb2d17dbab5a
Veselin Todorov <veselin@veselin.bg>
2012-03-10 16:44:38 +0200

    Add root route.

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,7 @@ Freedom::Application.routes.draw do
     delete 'sign_out', :to => 'user_sessions#destroy', :as => 'sign_out'
   end
 
+  root :to => 'authentication::user_sessions#new'
+
   get '/backdoor-login', :to => 'backdoor_login#login' if Rails.env.test?
 end
