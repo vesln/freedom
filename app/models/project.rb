ad2fad0c69a845285b42b0840f9ba9d687c1b52b
Veselin Todorov <veselin@veselin.bg>
2012-03-10 21:10:45 +0200

    Add `Milestones` association to `Project`.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
   attr_accessible :name, :description
+  has_many :milestones
   validates_presence_of :name
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Project do
+  it { should have_many(:milestones) }
   it { should validate_presence_of(:name) }
 
   it { should allow_mass_assignment_of(:name) }
