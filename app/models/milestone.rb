76fcdd05b9321a8797247538fb9843a6b2649140
Veselin Todorov <veselin@veselin.bg>
2012-03-10 20:08:13 +0200

    Add `project_id` validation in Miletones.

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -3,7 +3,7 @@ class Milestone < ActiveRecord::Base
   belongs_to :project
 
   validates_presence_of :name
-  # TODO: add validates_presence_of :project_id
+  validates_presence_of :project_id
 
   scope :for_project, lambda { |project| where(:project_id => project.id) }
 end
--- spec/models/milestone_spec.rb
+++ spec/models/milestone_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Milestone do
   it { should belong_to :project }
   it { should validate_presence_of :name }
+  it { should validate_presence_of :project_id }
 
   describe 'for_project' do
     it "return all milestones for a requested project" do
