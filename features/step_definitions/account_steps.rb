94048ef16c9f681a2e1856a1a31d6893f6239fb5
Veselin Todorov <veselin@veselin.bg>
2012-03-10 18:44:32 +0200

    Remove useless steps.

--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -1,16 +1,3 @@
-Given /^I am logged in with "([^"]*)"$/ do |email|
-  user = User.where(:email => email).first || FactoryGirl.create(:user, :email => email)
-  backdoor_login user
-end
-
-Then /^I should be to login with "([^"]*)" and "([^"]*)"$/ do |email, password|
-  UserSession.new(:email => email, :password => password).save.should === true
-end
-
-Given /^I am registered as "([^"]*)" and "([^"]*)"$/ do |email, password|
-  FactoryGirl.create(:user, :email => email, :password => password, :password_confirmation => password)
-end
-
 When /^I change my password to "([^"]*)"$/ do |password|
   fill_in 'Password', :with => password
   fill_in 'Password confirmation', :with => password
