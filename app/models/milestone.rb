ba228ca1683fe5a7ac97ec5c17ea8066b874094f
Veselin Todorov <veselin@veselin.bg>
2012-03-10 19:34:15 +0200

    Fix coding style.

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -5,5 +5,5 @@ class Milestone < ActiveRecord::Base
   validates_presence_of :name
   # TODO: add validates_presence_of :project_id
 
-  scope :for_project, lambda { |project| where :project_id => project.id }
+  scope :for_project, lambda { |project| where(:project_id => project.id) }
 end
