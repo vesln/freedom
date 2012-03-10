ab76084a6a84f96b5ae554be7365c166a1facc53
Veselin Todorov <veselin@veselin.bg>
2012-03-10 18:49:14 +0200

    Remove more backdoor login debris.

--- features/support/auth.rb
+++ features/support/auth.rb
@@ -1,9 +1,4 @@
 module Auth
-  def backdoor_login(user)
-    visit "/backdoor-login?email=#{CGI.escape(user.email)}"
-    @current_user = user
-  end
-
   def current_user=(user)
     puts 'current get called'
     @current_user = user
