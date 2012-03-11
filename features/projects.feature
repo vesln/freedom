f3e2d390ffbd2be43a81496c5654a8ccfd036681
Veselin Todorov <veselin@veselin.bg>
2012-03-11 08:56:36 +0200

    Add missing projects feature.

---
+++ features/projects.feature
@@ -0,0 +1,38 @@
+Feature: Projects
+
+  In order to organize tasks by projects
+  As a project owner
+  I want to manage projects
+
+  Background:
+    Given I am logged in
+
+  Scenario: View all projects
+    Given There are few existing projects
+    And I am on the projects page
+    Then I should see the projects
+
+  Scenario: Create a new project
+    Given I am on the projects page
+    When I follow "New project"
+    Then I create a new project with name "Project 1"
+    Then there should be a project "Project 1"
+
+  Scenario: Save a project with invalid data
+    Given I am on the projects page
+    And I follow "New project"
+    And I try to create a new project with invalid data
+    Then the project should not exist
+
+  Scenario: Edit a project
+    Given There are project "Project 1"
+    When I am on the projects page
+    And I edit the project "Project 1"
+    And I rename the project to "Project 2"
+    Then the project should be reanemd from "Project 1" to "Project 2"
+
+  Scenario: Delete a project
+    Given There are project "Project 1"
+    When I am on the projects page
+    And I delete the project "Project 1"
+    Then the project "0.1" should be deleted
--- features/step_definitions/projects_steps.rb
+++ features/step_definitions/projects_steps.rb
@@ -5,3 +5,58 @@
 # Veselin Todorov <hi@vesln.com>
 # MIT License
 #--
+
+Given 'There are few existing projects' do
+  1.upto(5) do |i|
+    FactoryGirl.create(:project, :name => 'Project ' << i.to_s)
+  end
+end
+
+Then 'I should see the projects' do
+  1.upto(5) do |i|
+    page.should have_content('Project ' << i.to_s)
+  end
+end
+
+Then /^I create a new project with name "([^"]*)"$/ do |name|
+  fill_in 'Name', :with => name
+  click_button 'Save'
+end
+
+Then /^there should be a project "([^"]*)"$/ do |name|
+  page.should have_content(name)
+end
+
+Given 'I try to create a new project with invalid data' do
+  click_button 'Save'
+end
+
+Then 'the project should not exist' do
+  page.should have_content('Sorry')
+end
+
+Given /^There are project "([^"]*)"$/ do |name|
+  FactoryGirl.create(:project, :name => name)
+end
+
+When /^I edit the project "([^"]*)"$/ do |name|
+  click_link 'Edit'
+end
+
+When /^I rename the project to "([^"]*)"$/ do |name|
+  fill_in 'Name', :with => name
+  click_button 'Save'
+end
+
+Then /^the project should be reanemd from "([^"]*)" to "([^"]*)"$/ do |old_name, new_name|
+  page.should have_content(new_name)
+  page.should_not have_content(old_name)
+end
+
+When /^I delete the project "([^"]*)"$/ do |name|
+  click_link 'Delete'
+end
+
+Then /^the project "([^"]*)" should be deleted$/ do |name|
+  page.should_not have_content(name)
+end
