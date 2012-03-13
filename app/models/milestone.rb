0a05d5981d19cc5a4ffd4dd23e05ad463538f215
Veselin Todorov <veselin@veselin.bg>
2012-03-13 07:51:04 +0200

    Milestones code formatting.

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -8,7 +8,11 @@
 
 class Milestone < ActiveRecord::Base
   attr_accessible :name
+
   belongs_to :project
+  has_many :tasks
+  has_many :completed_tasks, :class_name => 'Task', 
+           :conditions => {:state => Task::COMPLETED}
 
   validates_presence_of :name
   validates_presence_of :project_id
--- spec/models/milestone_spec.rb
+++ spec/models/milestone_spec.rb
@@ -9,10 +9,19 @@
 require 'spec_helper'
 
 describe Milestone do
+  it { should allow_mass_assignment_of(:name) }
+  it { should_not allow_mass_assignment_of(:project_id) }
+
   it { should belong_to(:project) }
+  it { should have_many(:tasks) }
+
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:project_id) }
 
-  it { should allow_mass_assignment_of(:name) }
-  it { should_not allow_mass_assignment_of(:project_id) }
+  it "has many completed tasks" do
+    milestone = create(:milestone)
+    create(:task, :milestone => milestone, :project_id => milestone.project_id)
+    completed = create(:completed_task, :milestone => milestone, :project_id => milestone.project_id)
+    milestone.reload.completed_tasks.all.should == [completed]
+  end
 end
