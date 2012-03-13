320fa0871c99de11f7ad6887add6fc97eb89c198
Veselin Todorov <veselin@veselin.bg>
2012-03-13 14:28:13 +0200

    Spec order: mass assignment, associations, validation.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,12 +9,12 @@
 require 'spec_helper'
 
 describe User do
-  it { should validate_presence_of :name }
-
   it { should allow_mass_assignment_of(:email) }
   it { should allow_mass_assignment_of(:name) }
   it { should allow_mass_assignment_of(:password) }
   it { should allow_mass_assignment_of(:password_confirmation) }
+
+  it { should validate_presence_of :name }
 
   context 'as registered' do
     it { should_not allow_mass_assignment_of(:email).as(:registered) }
