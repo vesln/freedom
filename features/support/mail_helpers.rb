69e41f635c31082085d0b1cd4457a036f346bcf4
Veselin Todorov <veselin@veselin.bg>
2012-03-14 11:19:33 +0200

    More copyright.

--- features/support/mail_helpers.rb
+++ features/support/mail_helpers.rb
@@ -1,3 +1,11 @@
+#++
+# Freedom - Mind-blowing issue tracker.
+#
+# http://github.com/vesln/freedom
+# Veselin Todorov <hi@vesln.com>
+# MIT License
+#--
+
 module MailHelpers
   def last_sent_email
     ActionMailer::Base.deliveries.last
