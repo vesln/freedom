356616c9409f46ff4fcb9e8f82fd11d4f00f0a6a
Veselin Todorov <veselin@veselin.bg>
2012-03-14 11:17:56 +0200

    Copyright.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,2 +1,10 @@
+#++
+# Freedom - Mind-blowing issue tracker.
+#
+# http://github.com/vesln/freedom
+# Veselin Todorov <hi@vesln.com>
+# MIT License
+#--
+
 class ApplicationMailer < ActionMailer::Base
 end
