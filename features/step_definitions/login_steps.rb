dca45d958f3e541b6b94e119e4dcdde4e23dd24c
Veselin Todorov <veselin@veselin.bg>
2012-03-18 17:26:41 +0200

    Stepdef reordering.

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -10,10 +10,6 @@ Given /^I am already registered with "([^"]*)" and "([^"]*)"$/ do |email, passwo
   FactoryGirl.create(:user, :email => email, :password => password, :password_confirmation => password)
 end
 
-Then "I should be logged in" do
-  page.should have_content('Logout')
-end
-
 When /^I enter valid login credentials "([^"]*)" and "([^"]*)"$/ do |email, password|
   fill_in 'Email', :with => email
   fill_in 'Password', :with => password
@@ -24,4 +20,8 @@ When /^I enter invalid password for "([^"]*)"$/ do |email|
   fill_in 'Email', :with => email
   fill_in 'Password', :with => 'wrong-wrong-wrong'
   click_button 'Sign in'
+end
+
+Then "I should be logged in" do
+  page.should have_content('Logout')
 end
