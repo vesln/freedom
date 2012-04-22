f10756162e035c9b80c74966f103d9f9c5d0a710
Veselin Todorov <veselin@veselin.bg>
2012-04-22 16:26:30 +0300

    Fix the spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,22 @@ require 'rubygems'
 require 'spork'
 
 Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
+
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  RSpec.configure do |config|
+    config.include Factory::Syntax::Methods
+    config.include EmailSpec::Helpers, type: :mailer
+    config.include EmailSpec::Matchers, type: :mailer
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+    config.use_transactional_fixtures = true
+    config.infer_base_class_for_anonymous_controllers = false
+    ActiveRecord::Observer.disable_observers
+  end
 end
 
 Spork.each_run do
@@ -17,19 +33,3 @@ Spork.each_run do
   Freedom::Application.reload_routes!
 end
 
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
-
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
-RSpec.configure do |config|
-  config.include Factory::Syntax::Methods
-  config.include EmailSpec::Helpers, type: :mailer
-  config.include EmailSpec::Matchers, type: :mailer
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
-  config.infer_base_class_for_anonymous_controllers = false
-  ActiveRecord::Observer.disable_observers
-end
